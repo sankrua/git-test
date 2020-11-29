5e7158ec2c22a0bb00825423b628952f35b58763
sankrua <1804287693@qq.com>
2020-11-29 19:32:05 +0800

    update

---
+++ git-test-project/src/com/sankrua/git/FirstClass.java
@@ -0,0 +1,24 @@
+package com.sankrua.git;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: sankrua
+ * Date: 2020/11/29
+ * Time: 18:16
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+    public void test(){
+        int i = 0;
+        i += 10;
+    }
+}
